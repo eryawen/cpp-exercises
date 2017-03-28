fa9294629cf91dd7ccb8fd486f8b97d929321e0c
Monika Kołtunik <monikakoltunik@gmail.com>
2017-03-28 23:24:39 +0200

    Adds factorial solution

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -2,7 +2,10 @@
 // Created by mwypych on 02.02.17.
 //
 #include "Factorial.h"
+#include <stdlib.h>
 
 int factorial(int value) {
-  return 0;
+    if (value <= -13 || value >= 13) return 0;
+    if (value == 0 || value == 1) return 1;
+    return value * factorial(value - abs(value) / value);
 }
