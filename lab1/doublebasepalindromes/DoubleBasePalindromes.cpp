7cdeab8a7668236ae1963b2c7d51379fbc1f5f37
mika6 <monikakoltunik@gmail.com>
2017-03-28 23:14:06 +0200

    Adds double base palindromes  solution skeleton

---
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.cpp
@@ -0,0 +1,10 @@
+//
+// Created by mk on 28.03.2017.
+//
+
+#include "DoubleBasePalindromes.h"
+
+uint64_t DoubleBasePalindromes(int max_vaule_exculsive) {
+    return 0;
+}
+//TODO execution time
---
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.h
@@ -0,0 +1,12 @@
+//
+// Created by mk on 28.03.2017.
+//
+
+#ifndef JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
+#define JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
+
+#include <cstdint>
+
+uint64_t DoubleBasePalindromes(int max_vaule_exculsive);
+
+#endif //JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
