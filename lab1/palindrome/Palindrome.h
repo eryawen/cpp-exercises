6de6448a1e0e8ce212723d7154cb705d5d4af7f6
mika6 <monikakoltunik@gmail.com>
2017-03-28 22:51:49 +0200

    Adds palindrome solution

---
+++ lab1/palindrome/Palindrome.h
@@ -0,0 +1,12 @@
+//
+// Created by mk on 26.03.2017.
+//
+
+#ifndef JIMP_EXERCISES_PALINDROME_H
+#define JIMP_EXERCISES_PALINDROME_H
+
+#include <string>
+
+bool is_palindrome(std::string str);
+
+#endif //JIMP_EXERCISES_PALINDROME_H
