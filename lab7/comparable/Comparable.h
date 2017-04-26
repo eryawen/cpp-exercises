9905fd41f8da98914bd44da1dd5cf70a056def2c
Monika Kołtunik <monikakoltunik@gmail.com>
2017-04-26 19:40:03 +0200

    Adds StudentComparator solution

--- lab7/comparable/Comparable.h
+++ lab7/comparable/Comparable.h
@@ -5,40 +5,39 @@
 #ifndef JIMP_EXERCISES_COMPARABLE_H
 #define JIMP_EXERCISES_COMPARABLE_H
 
+#include "../../lab6/academia/StudentRepository.h"
+
 namespace academia {
     class StudentComparator {
+    public:
         virtual bool IsLess(const Student &left, const Student &right) const = 0;
 
         bool operator()(const Student &left, const Student &right);
     };
 
     class ByFirstNameAscending : public StudentComparator {
+    public:
         bool IsLess(const Student &left, const Student &right) const override;
     };
 
     class ByFirstNameDescending : public StudentComparator {
+    public:
         bool IsLess(const Student &left, const Student &right) const override;
     };
 
     class ByLastNameAscending : public StudentComparator {
+    public:
         bool IsLess(const Student &left, const Student &right) const override;
     };
 
     class ByProgramAscendingEmptyFirst : public StudentComparator {
+    public:
         bool IsLess(const Student &left, const Student &right) const override;
     };
 
     class ByProgramAscendingEmptyLast : public StudentComparator {
+    public:
         bool IsLess(const Student &left, const Student &right) const override;
     };
 }
 #endif //JIMP_EXERCISES_COMPARABLE_H
-
-/*
- * ByFirstNameAscending
-ByFirstNameDescending
-ByLastNameAscending
-ByProgramAscendingEmptyFirst, porównuje kierunki studiów alfabetycznie ale przesuwa na początek nieustawiony program (string pusty)
-ByProgramAscendingEmptyLast, j.w. ale przesuwa na koniec nie ustawiony program studiów
-[1 plus] w programie main wykorzystać wybrany porównywacz do posortowania wektora studentów (algorytm sort z biblioteki algorithm)
- */
