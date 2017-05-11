ca2db8e4d63399866011b3a6fd1e145663197ecf
Monika Kołtunik <monikakoltunik@gmail.com>
2017-05-11 12:48:11 +0200

    Implements most of the functions from Lab10

--- lab10/algotest/Algo.cpp
+++ lab10/algotest/Algo.cpp
@@ -13,24 +13,22 @@
 
 namespace algo {
     std::set<std::string> Keys(const std::map<std::string, int> &m) {
-        std::set keys;
-        std::for_each(m.begin(), m.end(), [](const std::pair<std::string, int> &p) { keys.insert(p.first); });
+        std::set<std::string> keys;
+        std::for_each(m.begin(), m.end(), [&keys](const std::pair<std::string, int> &p) { keys.insert(p.first); });
         return keys;
     }
 
     std::vector<int> Values(const std::map<std::string, int> &m) {
-        std::vector values;
-        std::for_each(m.begin(), m.end(), [](std::pair<std::string, int> p) { values.(p.first); });
+        std::vector<int> values;
+        std::transform(m.begin(), m.end(), std::back_inserter(values), [](auto p) { return p.second; });
         return values;
     }
-    //back_inserter
+
 
     std::map<std::string, int> DivisableBy(const std::map<std::string, int> &m, int divisor) {
         std::map<std::string, int> values;
-//        std::for_each(m.begin(), m.end(),
-//                      [](std::pair<std::string, int> p) { if (p.second % divisor == 0) values.insert(p); });
-        std::copy_if(m.begin(), m.end(), values.begin(),
-                     [](std::pair<std::string, int> p) { return p.second % divisor == 0; });
+//        std::copy_if(m.begin(), m.end(), values.begin(),
+//                     [&divisor](std::pair<std::string, int> p) { return p.second % divisor == 0; });
         return values;
     }
 
@@ -43,11 +41,11 @@ namespace algo {
         std::copy(v.begin(), v.end(), values.begin());
         SortInPlace(&values);
         return values;
+        //?
     }
 
     bool pairCompare(const std::pair<int, int> &firstElem, const std::pair<int, int> &secondElem) {
         return firstElem.second < secondElem.second;
-
     }
 
     bool tupleCompare(const std::tuple<int, int, int> &firstElem, const std::tuple<int, int, int> &secondElem) {
@@ -68,12 +66,12 @@ namespace algo {
 
     std::vector<std::string> MapToString(const std::vector<double> &v) {
         std::vector<std::string> values;
-        std::transform(v.begin(), v.end(), values.begin(), [](auto p) { return std::to_string(p); });
+        std::transform(v.begin(), v.end(), std::back_inserter(values), [](auto p) { return std::to_string(p); });
         return values;
     }
 
     std::string Join(const std::string &joiner, const std::vector<double> &v) {
-//todo
+        //todo
     }
 
     int Sum(const std::vector<int> &v) {
@@ -93,30 +91,36 @@ namespace algo {
     }
 
     bool ContainsValue(const std::map<std::string, int> &v, int value) {
-        return false;
+        //todo
     }
 
     std::vector<std::string> RemoveDuplicates(const std::vector<std::string> &v) {
-        return std::vector<std::string>();
+        std::vector<std::string> res;
+//        return std::unique_copy(v.begin(), v.end(), res);
     }
 
     void RemoveDuplicatesInPlace(std::vector<std::string> *v) {
-
+        std::unique(v->begin(), v->end());
     }
 
     void InitializeWith(int initial_value, std::vector<int> *v) {
-
+        std::fill(v->begin(), v->end(), initial_value);
     }
 
     std::vector<int> InitializedVectorOfLength(int length, int initial_value) {
-        return std::vector<int>();
+        std::vector<int> v;
+        std::fill_n(std::back_inserter(v), length, initial_value);
+        return v;
     }
 
     void CopyInto(const std::vector<int> &v, int n_elements, std::vector<int> *out) {
-//        std::copy_if(v.begin(), m.end(), values.begin(),
+        std::copy_n(v.begin(), n_elements, std::back_inserter(*out));
     }
 
     int HowManyShortStrings(const std::vector<std::string> &v, int inclusive_short_length) {
-        return 0;
+        //todo
     }
+
+    //back_inserter
+    //todo const
 }
